f0f168d2f959da3b56baa1bf0ed9268b358e9193
Maia <contact@maia395.dev>
2026-06-25 05:20:28 +0100

    temp: Added a cuboid shape and a light

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(CameraControlsPlugin)
-        .add_systems(Startup, create_plane)
+        .add_systems(Startup, create_objects)
         .run();
 }
 
 
-fn create_plane(
+fn create_objects(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -22,4 +22,21 @@ fn create_plane(
         Mesh3d(meshes.add(Plane3d::default().mesh().size(5., 5.))),
         MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
     ));
+
+    // Cuboid
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::default().mesh())),
+        MeshMaterial3d(materials.add(Color::srgb(0.5, 0.3, 0.3))),
+        Transform::from_xyz(1., 0.5, 0.),
+    ));
+
+    // light
+    commands.spawn((
+        DirectionalLight {
+            shadow_maps_enabled: true,
+            ..Default::default()
+        },
+        Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
 }
