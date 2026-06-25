462399c84b1375fbf61140b08041455372cbee78
Maia <contact@maia395.dev>
2026-06-25 05:03:12 +0100

    feat: Added basic camera controls

--- src/camera_controls.rs
+++ src/camera_controls.rs
@@ -2,6 +2,9 @@ use bevy::{camera::ScalingMode, prelude::*};
 
 pub struct CameraControlsPlugin;
 
+#[derive(Component)]
+pub struct PrimaryCamera;
+
 fn add_camera(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
@@ -13,11 +16,38 @@ fn add_camera(mut commands: Commands) {
         }),
         Transform::from_xyz(30.0, 30.0, 30.0)
         .looking_at(Vec3::ZERO, Vec3::Y),
+        PrimaryCamera,
     ));
+}
+
+fn controls(
+    camera_query: Single<(&mut Camera, &mut Transform, &mut Projection)>,
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time<Fixed>>,
+) {
+    let (mut camera, mut transform, mut projection) = camera_query.into_inner();
+
+    let cam_speed = 1.0 * time.delta_secs();
+
+    // Camera movement controls
+    if input.pressed(KeyCode::KeyD) {
+        transform.translation.x += cam_speed;
+    }
+    if input.pressed(KeyCode::KeyA) {
+        transform.translation.x -= cam_speed;
+    }
+    if input.pressed(KeyCode::KeyS) {
+        transform.translation.z -= cam_speed;
+    }
+    if input.pressed(KeyCode::KeyW) {
+        transform.translation.z += cam_speed;
+    }
+
 }
 
 impl Plugin for CameraControlsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_camera);
+        app.add_systems(Update, controls);
     }
 }
