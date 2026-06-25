e1bdefa0b14f2871180e2d9ad439fbdd59081e57
Maia <contact@maia395.dev>
2026-06-25 03:42:53 +0100

    temp: Added testing plane to verify camera functions

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,14 @@ fn main() {
 }
 
 
+fn create_plane(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(20., 20.))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    ));
+}
